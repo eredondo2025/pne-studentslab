e4775235dff3274ff8e5585dd3be77fc4c21bd43
eredondo <e.redondo.2025@alumnos.urjc.es>
2026-02-04 18:11:18 +0100

    exercise 2 S02

---
+++ S01/S02/fiboN.py
@@ -0,0 +1,11 @@
+def fibon(n):
+    a = 0
+    b = 1
+    for i in range(n):
+        c = a + b
+        a = b
+        b = c
+    return a
+print("The 5th element is: ", fibon(5))
+print("The 10th element is: ", fibon(10))
+print("The 15th element is: ", fibon(15))
