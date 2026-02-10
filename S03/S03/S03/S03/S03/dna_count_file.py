4e1fa7335e25fb94ffd8ffb4d541b3432c574d24
eredondo <e.redondo.2025@alumnos.urjc.es>
2026-02-10 16:51:24 +0100

    exercise 3 S03

--- S03/S03/S03/S03/S03/dna_count_file.py
+++ S03/S03/S03/S03/S03/dna_count_file.py
@@ -1,17 +1,22 @@
-def count_dna(sequences):
+#Option 1
+#f = open("dna.txt", "r")
+#lines = f.read.lines()
+#f.close()
+#Option 2
+with open("dna.txt", "r") as f:
+    lines = f.readlines()
 
-    for seq in sequences:
-        print("The total lenght: ", len(seq))
-        bases = {"A": 0, "C": 0, "G": 0, "T": 0}
-        for base in seq:
-            if base in bases:
-                bases[base] += 1
+total_number = 0
+bases = {"A": 0, "C": 0, "G": 0, "T": 0}
+for seq in sequences:
+    sequence.strip()   #Remove spaces
+    total_number += len(sequence)
+
+    for base in seq:
+        if base in bases:
+            bases[base] += 1
+
+        print("The total number of bases: ", total_number)
 
         for base, count in bases.items():
             print(f'{base}: {count}')
-
-sequences = ["AGTACACTGGT",
-"ACCAGTGTACT",
-"ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG"]
-
-count_dna(sequences)
