4dd95c8236c1f8455ca431f19c164e03827c3fc0
eredondo <e.redondo.2025@alumnos.urjc.es>
2026-02-11 18:45:59 +0100

    exercise 6 basic exercise

--- Basic_exercise/exercise6.py
+++ Basic_exercise/exercise6.py
@@ -1,3 +1,4 @@
+#a
 def is_even(number):
     if number % 2 == 0:
         return "True"
@@ -5,7 +6,20 @@ def is_even(number):
         return "False"
 
 print("is_even(4) = ", is_even(4))
-print("is_even(7) = ", is_even(0))
+print("is_even(7) = ", is_even(7))
 print("is_even(0) = ", is_even(0))
 print("is_even(-3) = ", is_even(-3))
 print("is_even(10) = ", is_even(10))
+
+#b
+def classify_triangle(a, b, c):
+    if a == b == c:
+        return "equilateral"
+    elif a == b or b == c or a == c:
+        return "isosceles"
+    elif a != b != c:
+        return "scalene"
+
+print(classify_triangle(5, 5, 5))
+print(classify_triangle(3, 3, 4))
+print(classify_triangle(3, 4, 5))
