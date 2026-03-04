14c14c53da8a7e401199ca260d36545cad675d6d
eredondo <e.redondo.2025@alumnos.urjc.es>
2026-03-04 18:21:05 +0100

    PRACTICE 2

---
+++ P02/e4.py
@@ -0,0 +1,24 @@
+from Seq1 import Seq
+from Client0 import Client
+
+PRACTICE = 2
+EXERCISE = 4
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+# -- Parameters of the server to talk to
+IP = "212.128.255.71" # your IP address
+PORT = 8080
+
+# -- Create a client object
+c = Client(IP, PORT)
+
+s = Seq()
+FOLDER = "../sequences/"
+gene_list = ["U5.txt", "ADA.txt", "FRAT1.txt", "FXN.txt", "RNU6_269P.txt"]
+for gene in gene_list:
+    s.read_fasta(FOLDER + gene)
+    print(f"Sending the {gene} Gene to the server...")
+    response = c.talk(str(s))
+    print(f"To server: {str(s)}")
+    print(f"From server: {response}")
