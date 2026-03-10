4e892340be3f2165468848d8c069c595af91db83
eredondo <e.redondo.2025@alumnos.urjc.es>
2026-03-10 15:53:09 +0100

    SESION 10

--- S10/happy-server.py
+++ S10/happy-server.py
@@ -1,11 +1,20 @@
 import socket
 
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
 # Configure the Server's IP and PORT
 PORT = 8080
-IP = "212.128.255.91" # the IP address depends on the machine running the server
+IP = "212.128.255.91"
 
 # -- Step 1: create the socket
 ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # -- Step 2: Bind the socket to server's IP and PORT
 ls.bind((IP, PORT))
@@ -15,11 +24,45 @@ ls.listen()
 
 print("The server is configured!")
 
-# -- Waits for a client to connect
-print("Waiting for Clients to connect")
-ls.accept()
+while True:
+    # -- Waits for a client to connect
+    print("Waiting for Clients to connect")
 
-print("A client has connected to the server!")
+    try:
+        (cs, client_ip_port) = ls.accept()
 
-# -- Close the socket
-ls.close()
+    # -- Server stopped manually
+    except KeyboardInterrupt:
+        print("Server stopped by the user")
+
+        # -- Close the listenning socket
+        ls.close()
+
+        # -- Exit!
+        exit()
+
+    # -- Execute this part if there are no errors
+    else:
+
+        print("A client has connected to the server!")
+
+        # -- Read the message from the client
+        # -- The received message is in raw bytes
+        msg_raw = cs.recv(2048)
+
+        # -- We decode it for converting it
+        # -- into a human-redeable string
+        msg = msg_raw.decode()
+
+        # -- Print the received message
+        print(f"Message received: {msg}")
+
+        # -- Send a response message to the client
+        response = "HELLO. I am the Happy Server :-)\n"
+
+        # -- The message has to be encoded into bytes
+        cs.send(response.encode())
+
+        # -- Close the data socket
+        cs.close()
+
